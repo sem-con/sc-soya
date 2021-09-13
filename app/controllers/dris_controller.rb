17f4d8605a65c8f9623d845f64ea27e073a18799
Christoph Fabianek <christoph.fabianek@gmail.com>
2021-09-13 15:54:19 +0200

    update DRI calculation

--- app/controllers/dris_controller.rb
+++ app/controllers/dris_controller.rb
@@ -7,13 +7,13 @@ class DrisController < ApplicationController
     respond_to :xml, only: []
 
     def read
-       @item = Store.find_by_dri(params[:dri]) rescue nil
-       if @item.nil?
+        @item = Store.find_by_dri(params[:dri]) rescue nil
+        if @item.nil?
             render json: {"error": "not found"},
                     status: 404
             return
         end
-       render json: JSON.parse(@item.item),
-              status: 200
+        render json: JSON.parse(@item.item),
+               status: 200
     end
 end
--- test/dri.rb
+++ test/dri.rb
@@ -9,16 +9,49 @@ require 'digest'
 require 'json/canonicalization'
 require 'optparse'
 
+def checkDRI(val)
+    pos = (val =~ /[1-9A-HJ-NP-Za-km-z]{47}/)
+    if pos.nil?
+      return val
+    else
+      return val[pos..(pos+46)]
+    end
+end
+
+def iterate(i)
+  if i.is_a?(Hash)
+    i.each do |k, v|
+      if v.is_a?(Hash) || v.is_a?(Array)
+        iterate(v)
+      else
+        i[k] = checkDRI(v)
+      end
+    end
+  elsif i.is_a?(Array)
+    i.map! do |v|
+      if v.is_a?(String)
+        checkDRI(v)
+      else
+        iterate(v)
+      end
+    end
+  end
+end
+
 # commandline options
 options = { }
+options[:verbose] = false
 opt_parser = OptionParser.new do |opt|
 
   options[:doc_type] = 0 # 0 = layer
-  opt.on("-l","--layer","document is a layer (base or overlay)") do |v|
+  opt.on("-l", "--layer", "document is a layer (base or overlay)") do |v|
     options[:doc_type] = 0
   end
-  opt.on("-i","--instance","document is an instance") do |v|
+  opt.on("-i", "--instance", "document is an instance") do |v|
     options[:doc_type] = 1 # 1 = instance
+  end
+  opt.on("-v", "--verbose", "print additional processing information") do |v|
+    options[:verbose] = true
   end
 end
 opt_parser.parse!
@@ -35,4 +68,9 @@ else
 	# document is an instance
 	raw["@graph"].map{|i| i.delete("@id") rescue nil} rescue nil
 end
-puts Multibases.pack("base58btc", Multihashes.encode(Digest::SHA256.digest(raw.to_json_c14n), "sha2-256").unpack('C*')).to_s
+raw = iterate(JSON.parse(raw.to_json_c14n)).to_json_c14n
+if options[:verbose]
+  puts "Raw input"
+  puts raw.to_s
+end
+puts Multibases.pack("base58btc", Multihashes.encode(Digest::SHA256.digest(raw), "sha2-256").unpack('C*')).to_s
