8cb3d17e1b622aed3abf72e799ad45eac973407d
Christoph Fabianek <christoph.fabianek@gmail.com>
2021-11-16 09:28:40 +0100

    add White Paper

--- test/run.sh
+++ test/run.sh
@@ -1,22 +1,22 @@
 #!/usr/bin/env bash
 
 # test pushing a YML
-cat checks/person_test.yml | soya init | soya push > tmp.doc
-if ! cmp -s tmp.doc checks/push.doc ; then
+cat inputs/person_test.yml | soya init | soya push > tmp.doc
+if ! cmp -s tmp.doc checks/push.txt ; then
 	echo "error: pushing base layer from YML failed"
 	rm tmp.doc
 	exit 1
 fi
 
-cat checks/person_validation_test.yml | soya init | soya push > tmp.doc
-if ! cmp -s tmp.doc checks/push_overlay.doc ; then
+cat inputs/person_validation_test.yml | soya init | soya push > tmp.doc
+if ! cmp -s tmp.doc checks/push_overlay.txt ; then
 	echo "error: pushing overlay layer from YML failed"
 	rm tmp.doc
 	exit 1
 fi
 
 soya pull PersonOverlay_Test > tmp.doc
-if ! cmp -s tmp.doc checks/person_overlay.jsonld ; then
+if ! cmp -s tmp.doc inputs/person_overlay.jsonld ; then
 	echo "error: validating overlay layer after push failed"
 	rm tmp.doc
 	exit 1
@@ -27,7 +27,7 @@ rm tmp.doc
 
 # test validating flat JSON
 echo '{"hello": "world"}' | soya validate Person_Test > tmp.doc
-if ! cmp -s tmp.doc checks/flat.doc ; then
+if ! cmp -s tmp.doc checks/flat.txt ; then
 	echo "error: validating flat JSON failed"
 	rm tmp.doc
 	exit 1
@@ -64,9 +64,9 @@ fi
 rm tmp.doc
 
 # test transformation overlay
-cat checks/person_a.yml | soya init | soya push > /dev/null
-cat checks/person_b.yml | soya init | soya push > /dev/null
-cat checks/person_b_jolt.yml | soya init | soya push > /dev/null
+cat inputs/person_a.yml | soya init | soya push > /dev/null
+cat inputs/person_b.yml | soya init | soya push > /dev/null
+cat inputs/person_b_jolt.yml | soya init | soya push > /dev/null
 
 curl -k -s https://playground.data-container.net/PersonAinstance | soya transform PersonB | soya validate PersonB > tmp.doc
 if ! cmp -s tmp.doc checks/PersonBjq.json ; then
